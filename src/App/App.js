35f7b808a4280e2d664f33bd38dd6fbac3e96919
Kelsie Besinger <kelsiebesinger@gmail.com>
2021-02-17 09:20:11 -0700

    Add Reservation component to App component

--- src/App/App.js
+++ src/App/App.js
@@ -1,16 +1,22 @@
 import React, { Component } from 'react';
 import './App.css';
+import Reservations from '../Reservations/Reservations'
 
 class App extends Component {
+  constructor() {
+    super()
+    this.state = {
+
+    }
+  }
   render() {
     return (
       <div className="App">
         <h1 className='app-title'>Turing Cafe Reservations</h1>
         <div className='resy-form'>
-
         </div>
         <div className='resy-container'>
-          
+          <Reservations />
         </div>
       </div>
     )
