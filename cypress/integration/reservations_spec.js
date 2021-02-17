e8ef8517a49abef0e383379837551c8858c2551c
Kelsie Besinger <kelsiebesinger@gmail.com>
2021-02-17 10:13:12 -0700

    Add test to assert reservation button can be clicked

--- cypress/integration/reservations_spec.js
+++ cypress/integration/reservations_spec.js
@@ -28,12 +28,15 @@ describe("Turing Cafe", () => {
           .should("have.value", "2")
     })
 
+    it('Should be able to click the reservation button', () => {
+        cy.get('form button').click()
+    })
+
    
     
     // reservation cards
 
     //Write tests covering what should be displayed on the page when the user first visits.
-// Write a test that checks that when data is put into the form, the value is reflected in that form input.
 // Write a test to check the user flow of adding a new reservation to the page.
 
 
