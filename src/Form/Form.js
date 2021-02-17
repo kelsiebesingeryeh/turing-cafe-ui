423ddb5d19998d05eca3352532cb11e7a89d0ce8
Kelsie Besinger <kelsiebesinger@gmail.com>
2021-02-17 09:49:22 -0700

    Add handle change functionality to update the apps state

--- src/Form/Form.js
+++ src/Form/Form.js
@@ -11,8 +11,10 @@ class Form extends Component {
         }
     }
 
-    handleChange = () => {
-
+    handleChange = (event) => {
+        this.setState({
+            [event.target.name]: event.target.value
+        })
     }
 
 
@@ -47,6 +49,7 @@ class Form extends Component {
               value={this.state.number}
               onChange={this.handleChange}
             />
+          <button>Make Reservation</button>
           </form>
         );
     }
