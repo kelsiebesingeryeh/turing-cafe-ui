fe717a299b9ddd1bbd609c95698eda2b7362ff3f
Kelsie Besinger <kelsiebesinger@gmail.com>
2021-02-17 09:42:50 -0700

    Add Form component and add state for Form

---
+++ src/Form/Form.js
@@ -0,0 +1,18 @@
+import React, { Component } from 'react'
+
+class Form extends Component {
+    constructor() {
+        super()
+        this.state = {
+            id: Date.now(),
+            name: '',
+            time: '',
+            number: ''
+        }
+    }
+    render() {
+
+    }
+}
+
+export default Form
